b7fab8da79557d3e7f1c8b1eb944e3d4f600235e
Tair <tairtopskiy@gmail.com>
2024-04-09 21:30:48 +0500

    header layout added

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,32 @@
 import Image from 'next/image'
+import styles from './Header.module.css'
 
 function Header(): JSX.Element {
   return (
-    <header>
-      <Image
-        src={'/images/logo.svg'}
-        alt={''}
-        width={100}
-        height={60}
-      />
+    <header className={styles.block}>
+      <div className={styles.leftSide}>
+        <Image src={'/images/logo.svg'} alt={'logo'} width={100} height={60} />
+        <Image
+          src={'/images/icons/settings.svg'}
+          alt={'logo'}
+          width={20}
+          height={20}
+        />
+        <Image
+          src={'/images/icons/code.svg'}
+          alt={'logo'}
+          width={20}
+          height={20}
+        />
+      </div>
+      <div className={styles.rightSide}>
+        <Image
+          src={'/images/icons/user.svg'}
+          alt={'logo'}
+          width={20}
+          height={20}
+        />
+      </div>
     </header>
   )
 }
---
+++ src/ui/Icon.tsx
@@ -0,0 +1,3 @@
+function Icon(): JSX.Element {
+    return <svg></svg>
+}
