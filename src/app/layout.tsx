e1009728e8a3496ad4fc570d1d7db45ec411bb75
Tair <tairtopskiy@gmail.com>
2024-04-15 23:15:52 +0500

    metadata added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,13 @@ const monserat = Montserrat({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Typo - Master your typing skills',
-  description: "Hi, let's connect https://www.linkedin.com/in/tair-sairanbekov/",
+  description:
+    "Hi, let's connect https://www.linkedin.com/in/tair-sairanbekov/",
+  authors: {
+    name: 'Tair Sairanbekov',
+    url: 'https://www.linkedin.com/in/tair-sairanbekov/',
+  },
+  icons: '/images/Keyboard.svg'
 }
 
 export default function RootLayout({
